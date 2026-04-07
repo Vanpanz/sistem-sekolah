5763f5e0ea8cf95fe39fb9135a4313c71645e8c9
Vanpanz <fransiskusevan0609@gmail.com>
2026-04-07 12:24:24 +0700

    feat: implement submit button and back button in student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -28,8 +28,8 @@
         <!-- Card Header Start-->
 
         <!-- Card Body Start-->
-    <div class="bg-white shadow rounded-lg p-4">
-        <form action="" class ="grid grid-cols-2 gap-4">
+    <div class="bg-white shadow rounded-lg p-4"> 
+        <form action="" class ="grid grid-cols-2 gap-4"> 
             <div class ="space-y-2">
                 <label class ="block font-bold" for="name">Nama</label>
                 <input class="w-full px-4 py-2 border rounded-lg" type ="text" id="name" placeholder="Masukkan Nama" name="name">
@@ -46,13 +46,18 @@
             </div>
 
             <div class ="space-y-2">
-                <label class ="block font-bold" for="phone_number">Kelas</label>
+                <label class ="block font-bold" for="phone_number">Nomor Telepon</label>
                 <input class="w-full px-4 py-2 border rounded-lg" type ="text" id="phone_number" placeholder="Masukkan Nomor Telepon" name="phone_number">
             </div>
-</div>
+            <div class="flex justify-end col-span-2 gap-4"> 
+                <a href="/students" class="py-2 px-4 bg-gray-200 rounded-lg">Kembali</a>
+                <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
+            </div>
+        </form>
+    </div>
         <!-- Card Body End-->
     </main>
-     <!-- Main End-->
+    <!-- Main End-->
 
     <!-- Footer Start-->
     <footer class="bg-gray-900 text-white">
