62bb39d4fef258c1b997a6a4db9c0480fd9130fc
Vanpanz <fransiskusevan0609@gmail.com>
2026-04-09 13:11:02 +0700

    feat: improvement view

---
+++ app/core/Controller.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Core;
+
+class Controller 
+{
+
+    public function view(string $view, array $data = [])
+    {
+
+        extract($data);
+
+        $view = str_ireplace
+        (
+            '.',
+            '/',
+            $view
+        );
+
+        require_once "../app/views/{$view}.php";
+
+    }
+
+}
+
+?>
